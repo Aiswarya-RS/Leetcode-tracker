e55201d0c8ebfb7f45a6a00356ee3259d95e0579
Aiswarya R S <aiswaryaram025@gmail.com>
2026-07-09 09:16:52 +0530

    Create 2563-SplitMessageBasedOnLimit

---
+++ 2563-SplitMessageBasedOnLimit/2563-SplitMessageBasedOnLimit.java
@@ -0,0 +1,31 @@
+// Last updated: 09/07/2026, 09:16:53
+class Solution {
+    private int getPages(int l, int limit) {
+        int curLen = limit - 5; 
+        int curCapacity = 9;
+        int extraSpace = 0;
+        while (l > curLen * curCapacity + extraSpace) {
+            curLen -= 2;
+            extraSpace += curCapacity; 
+            curCapacity = Integer.parseInt(curCapacity + "9");
+            if (curLen <= 0) return 0;
+        }
+        int ans = (l - extraSpace) / curLen;
+        if ((l - extraSpace) % curLen != 0) ++ans;
+        return ans;
+    }
+
+    public String[] splitMessage(String message, int limit) {
+        int l = message.length();
+        int pages = getPages(l, limit);
+        String[] ans = new String[pages];
+        int curPos = 0;
+        for (int i=0; i<pages; ++i) {
+            String tag = "<" + (i+1) + "/" + pages + ">";
+            int infoLen = limit - tag.length();
+            ans[i] = message.substring(curPos, Math.min(l, curPos + infoLen)) + tag;
+            curPos += infoLen;
+        }
+        return ans;
+    }
+}
