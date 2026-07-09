ec597100c6474852426625994a948a5cd332989f
Aiswarya R S <aiswaryaram025@gmail.com>
2026-07-09 09:19:44 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 09:19:46
+class Solution {
+    public int rob(int[] nums) {
+        int prev1 = 0;
+        int prev2 = 0;
+
+        for (int i=0;i<nums.length;i++) {
+            int pick = prev2 + nums[i];
+            int notPick = prev1;
+
+            int current = Math.max(pick, notPick);
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+}
