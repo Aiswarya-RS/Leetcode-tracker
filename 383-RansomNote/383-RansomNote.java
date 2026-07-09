c1b92f786d8e1a0f53e1882c76dc1315e5accf5e
Aiswarya R S <aiswaryaram025@gmail.com>
2026-07-09 09:18:46 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 09:18:47
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int count[]=new int[26];
+        for(int i=0;i<magazine.length();i++)
+        {
+            count[magazine.charAt(i)-'a']++;
+        }
+        for(int i=0;i<ransomNote.length();i++)
+        {
+            count[ransomNote.charAt(i)-'a']--;
+            if(count[ransomNote.charAt(i)-'a']<0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
